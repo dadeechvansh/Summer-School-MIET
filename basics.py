6ac7a5d3d42e6bf396b749d934391bdce47848dd
dadeechvansh <dadeechvansh@gmail.com>
2026-06-24 12:45:58 +0530

    few more changes

--- basics.py
+++ basics.py
@@ -366,3 +366,13 @@ print(collection)
 
 collection.clear()
 print(collection)
+
+#union
+set1={1,2,3,4}
+set2={5,6,7,8}
+
+set3=set1.union(set2)
+print(set3)
+
+set4=set1.intersection(set2)
+print(set4)
